556b100836ca2949ed6c38600bbf7ba1c3dbc357
darq <prototypid@gmail.com>
2023-11-20 14:49:26 +0530

    feat: middleware that accepts schema & parses req

---
+++ src/validateResource.ts
@@ -0,0 +1,23 @@
+/* 
+When a request comes in, we're going to provide a schema and it's going to validate the request against that schema
+*/
+import { AnyZodObject } from "zod";
+import { NextFunction, Request, Response } from "express";
+import log from "./utils/logger";
+
+const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+    } catch (err: any) {
+      log.error(err);
+      return res.status(400).send(err.errors);
+    }
+  };
+
+export default validate;
