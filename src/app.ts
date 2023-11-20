42d32aa0f03541e7c2f5e38579882371aa08be38
darq <prototypid@gmail.com>
2023-11-20 09:39:12 +0530

    feat: Add routes file & healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import config from "config";
 import express from "express";
 
+import routes from "./routes";
 import log from "./utils/logger";
 import connectDb from "./utils/connectDB";
 
@@ -10,5 +11,8 @@ const app = express();
 
 app.listen(port, async () => {
   log.info(`App is listening at port ${port}`);
+
   await connectDb();
+
+  routes(app);
 });
