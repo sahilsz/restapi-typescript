edae5e4a0780e0437009cbc645a4a1246f75e291
darq <prototypid@gmail.com>
2023-11-20 13:32:41 +0530

    feat: Add user mongoose model

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import config from "config";
+import mongoose from "mongoose";
+
+// TypeScript defination
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      uniquer: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
