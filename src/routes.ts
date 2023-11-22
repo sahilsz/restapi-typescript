3ed9a6883953886240c075fccd9944b36ec592f1
darq <prototypid@gmail.com>
2023-11-22 11:17:15 +0530

    feat: Add route to create new User

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validate from "./validateResource";
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  // Register User
+  app.post("/api/users", validate(createUserSchema), createUserHandler);
 }
 
 export default routes;
