9323fac00040aab0678116cc83ef9bd132cfd2ab
darq <prototypid@gmail.com>
2023-11-22 11:16:29 +0530

    fix:: Omit password from User object & return object

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,9 +1,13 @@
-import User, { UserDocument } from "../models/user.model";
+import User, { UserInput } from "../models/user.model";
+import { omit } from "lodash";
 
-export async function createUser(input: UserDocument) {
+export async function createUser(input: UserInput) {
   try {
-    return await User.create(input);
+    const user = await User.create(input);
+
+    return omit(user.toJSON(), "password");
   } catch (err: any) {
+    console.log(err);
     throw new Error(err);
   }
 }
