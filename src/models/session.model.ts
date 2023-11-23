59574cf67a867cb26d05e82afa85a3daf42372f0
darq <prototypid@gmail.com>
2023-11-23 09:47:34 +0530

    feat: Add Session mongoose model

---
+++ src/models/session.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+import { UserDocument } from "./user.model";
+import { string } from "zod";
+
+export interface SessionDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Session = mongoose.model("Session", sessionSchema);
+
+export default Session;
