fde1ae2ad359e43bdd3dc7af9183077e08d1b918
darq <prototypid@gmail.com>
2023-11-27 08:24:14 +0530

    feat: Add route to create new session

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,24 @@
 import { Express, Request, Response } from "express";
-import { createUserHandler } from "./controller/user.controller";
-import validate from "./validateResource";
+
+import validate from "./middleware/validateResource";
+import sessionSchema from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
+import { createUserHandler } from "./controller/user.controller";
+import {
+  createUserSessionHandler,
+  getUserSessionsHandler,
+} from "./controller/session.controller";
+import requireUser from "./middleware/requireUser";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   // Register User
   app.post("/api/users", validate(createUserSchema), createUserHandler);
+
+  // Login
+  app.post("/api/sessions", validate(sessionSchema), createUserSessionHandler);
+
 }
 
 export default routes;
