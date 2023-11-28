2ded5f49833a513754345b985a9a34ea1aad60be
darq <prototypid@gmail.com>
2023-11-28 08:06:12 +0530

    feat: Add deserialze middleware to get user id from access token

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import express from "express";
 import routes from "./routes";
 import log from "./utils/logger";
 import connectDb from "./utils/connectDB";
+import deserializeUser from "./middleware/deserializeUser";
 
 const port = config.get<number>("port");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(deserializeUser);
 
 app.listen(port, async () => {
   log.info(`App is listening at port ${port}`);
