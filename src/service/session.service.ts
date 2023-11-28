7a1e9b75e145b4ee8894a05381647d62724379db
darq <prototypid@gmail.com>
2023-11-28 08:20:23 +0530

    feat: Add session service to update session

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,4 +1,4 @@
-import { FilterQuery } from "mongoose";
+import { FilterQuery, UpdateQuery } from "mongoose";
 
 import Session, { SessionDocument } from "../models/session.model";
 
@@ -10,4 +10,11 @@ export async function createUserSession(userId: string, userAgent: string) {
 
 export async function findSessions(query: FilterQuery<SessionDocument>) {
   return Session.find(query).lean(); // lean returns the plain object
+}
+
+export async function updateSession(
+  query: FilterQuery<SessionDocument>,
+  update: UpdateQuery<SessionDocument>
+) {
+  return Session.updateOne(query, update);
 }
