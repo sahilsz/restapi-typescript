c973af361f8e4c66e4e1eef18e9f9b9b4748a270
darq <prototypid@gmail.com>
2023-11-29 12:22:32 +0530

    feat: ReIssue token if access token is expired

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -2,12 +2,19 @@
 import { get } from "lodash"; // get makes it easier to access a property that we're not sure exists on the object
 import { Request, Response, NextFunction } from "express";
 import { verifyJwt } from "../utils/jwt.utils";
+import { reIssueAccessToken } from "../service/session.service";
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const accessToken = get(req, "headers.authorization", "").replace(
     /^Bearer\s/,
     ""
   );
+
+  const refreshToken = get(req, "headers.x-refresh") as string;
 
   if (!accessToken) return next();
 
@@ -15,6 +22,22 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
 
   if (decoded) {
     res.locals.user = decoded;
+    return next();
+  }
+
+  // re-issue token
+  if (refreshToken && expired) {
+    const newAcessToken = await reIssueAccessToken({
+      refreshToken,
+    });
+
+    if (newAcessToken) {
+      res.setHeader("x-access-token", newAcessToken);
+    }
+
+    const result = verifyJwt(newAcessToken as string);
+    res.locals.user = result.decoded;
+
     return next();
   }
 };
