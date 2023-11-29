b581bb5800b887fd1f4758aed078de7a43aa698d
darq <prototypid@gmail.com>
2023-11-29 12:23:38 +0530

    feat: ReIssue access token

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,6 +1,10 @@
+import config from "config";
+import { get } from "lodash";
 import { FilterQuery, UpdateQuery } from "mongoose";
 
 import Session, { SessionDocument } from "../models/session.model";
+import { signJwt, verifyJwt } from "../utils/jwt.utils";
+import { findUser } from "./user.service";
 
 export async function createUserSession(userId: string, userAgent: string) {
   const session = await Session.create({ user: userId, userAgent });
@@ -17,4 +21,29 @@ export async function updateSession(
   update: UpdateQuery<SessionDocument>
 ) {
   return Session.updateOne(query, update);
+}
+
+export async function reIssueAccessToken({
+  refreshToken,
+}: {
+  refreshToken: string;
+}) {
+  const { decoded } = verifyJwt(refreshToken);
+
+  if (!decoded || !get(decoded, "session")) return false;
+
+  const session = await Session.findById(get(decoded, "session"));
+
+  if (!session || !session.valid) return false;
+
+  const user = await findUser({ _id: session.user });
+
+  if (!user) return false;
+
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTTL") }
+  );
+
+  return accessToken;
 }
